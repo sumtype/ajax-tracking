1ae65faffe9e5c731633803e749f1fe01aab57a2
sumtype <james@sumtype.tech>
2018-01-29 05:49:17 -0500

    Added tracking script.  Works okay in a perfect world, of which ours is partly.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ajax-tracking
-Track Ajax requests on a page to determine when it's loaded.
+# Ajax Tracking
+
+Script to track Ajax requests on a page to determine when it's done loading.  Provides a function to the global namespace, `callWhenReadyToGo`, that takes a callback function which is called when the window has loaded and there are no pending Ajax requests.
+
+### Known Issues
+
+Doesn't always work when the page involves requests made via some higher-level libraries.  In general, if the request is performed using the `XMLHttpRequest` API and concludes (successfully or not) with an `onreadystatechange` event changing the `readyState` value of the XHR request to the value of `XMLHttpRequest.DONE` the tracker understands the XHR request to no longer be pending.  If `onreadystatechange` is not fired, or `readyState` is not ultimately set to the value of `XMLHttpRequest.DONE` the tracker will indefinitely consider the request to be pending so will not consider the page to have ever loaded.
---
+++ ajax-tracking.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Defines properties on window to avoid additional global variables (except the "callWhenReadyToGo" function).
+window._AjaxTracking = {
+  XMLHttpRequestTracker: [0],
+  XMLHttpRequestOpen: window.XMLHttpRequest.prototype.open,
+  windowLoaded: function windowLoaded() {
+    window._AjaxTracking.XMLHttpRequestTracker[0] = window.XMLHttpRequest.DONE;
+  },
+  XMLHttpRequestTrackingCheckFrequency: 100
+};
+
+// Overrides the default XMLHttpRequest open() function to track the status of Ajax requests via an array of integers corresponding to .
+window.XMLHttpRequest.prototype.open = function() {
+  var index = window._AjaxTracking.XMLHttpRequestTracker.slice().length, onReadyStateChange = this.onreadystatechange;
+  this.onreadystatechange = function() {
+    window._AjaxTracking.XMLHttpRequestTracker[index] = this.readyState;
+    if (this.readyState === window.XMLHttpRequest.DONE) window._AjaxTracking.XMLHttpRequestTracker[index] = this.readyState;
+    if (onReadyStateChange !== null) onReadyStateChange.apply(this, arguments);
+  }
+  window._AjaxTracking.XMLHttpRequestOpen.apply(this, arguments);
+}
+
+// Sets an event listener to determine when page assets have loaded.
+window.addEventListener('load', function() { window._AjaxTracking.windowLoaded(); });
+
+// Function that takes a calls an input function when the window has loaded and there are no pending Ajax requests.
+function callWhenReadyToGo(callback) {
+  var ajaxTracking = setInterval(function() {
+    if (!window._AjaxTracking.XMLHttpRequestTracker.filter(function(n) { return n !== 4; }).length) {
+      clearInterval(ajaxTracking);
+      callback();
+    }
+  }, window._AjaxTracking.XMLHttpRequestTrackingCheckFrequency);
+}
+
+callWhenReadyToGo(function() { console.log('Loading complete.') })
